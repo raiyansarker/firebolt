26089e880632e6bd1ffaac4a9b1496d87d18baa9
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-02 01:35:04 +0600

    fix: remove logging for the time being

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,5 @@
 import { authHandler } from '$lib/server/hooks/auth';
-import { reqLogger as logger } from '$lib/server/hooks/log';
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle = sequence(logger, authHandler) satisfies Handle;
+export const handle = sequence(authHandler) satisfies Handle;
