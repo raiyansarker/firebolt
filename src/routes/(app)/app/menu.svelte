85a2d31dedf8af2877f0852859421d623f06f83b
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-03 16:40:47 +0600

    feat: add escape key functionality to menu component

--- src/routes/(app)/app/menu.svelte
+++ src/routes/(app)/app/menu.svelte
@@ -39,11 +39,12 @@
 	});
 </script>
 
+<svelte:window on:keydown={(e) => e.key === "Escape" && (menuOpen = false)} />
 <div
 	transition:slide={{ duration: 300, axis: "y", easing: linear }}
-	class="absolute inset-0 z-30 space-y-4 bg-background px-4 py-6 md:hidden"
+	class="absolute inset-0 z-30 space-y-4 bg-highlight px-4 py-6 text-background/70 md:hidden"
 >
-	<div class="flex flex-row items-center justify-between border-b-[1] border-b-border px-2">
+	<div class="flex flex-row items-center justify-between px-2">
 		<h1 class="text-xl font-bold tracking-wide">{env.PUBLIC_APP_NAME}</h1>
 		<button
 			bind:this={closeButton}
@@ -56,7 +57,7 @@
 	<nav class="flex flex-col items-start justify-center gap-y-1">
 		{#each [...menuItems, { name: "Settings", href: "/app/settings" }] as item}
 			<a
-				class="w-full rounded-sm px-2 py-1 font-medium outline-none ring-ring transition-colors hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground focus-visible:ring-1"
+				class="w-full rounded-sm px-2 py-1 font-medium outline-none ring-ring transition-colors hover:bg-muted hover:text-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground focus-visible:ring-1"
 				class:mainmenu__nav_link_active={item.href === $page.url.pathname}
 				href={item.href}
 			>
