2292157ce63794cb70738a40c8ea149062a339eb
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-05 23:47:34 +0600

    fix: remove console log statement from +layout.server.ts

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -6,8 +6,6 @@ export const load: LayoutServerLoad = async (event) => {
 	const matcher = ["/app/:path*"];
 	const session = await event.locals.getSession();
 
-	console.log(event.url.pathname, pathToRegexp(matcher).test(event.url.pathname));
-
 	if (pathToRegexp(matcher).test(event.url.pathname)) {
 		if (!session) throw redirect(307, "/login");
 	}
