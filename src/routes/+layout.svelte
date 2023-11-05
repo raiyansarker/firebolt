0336f1855db9557395dc1888bc5c82835d98cec7
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-05 20:00:02 +0600

    feat: add sonner toast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import '../styles/tailwind.postcss';
+	import "../styles/tailwind.postcss";
+	import { Toaster } from "svelte-sonner";
 </script>
 
+<Toaster closeButton position="bottom-right" />
 <slot />
