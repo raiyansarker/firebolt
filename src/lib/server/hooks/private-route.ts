6b24324ded8da09ff5caa344d74ba30c979ba2fe
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-05 21:25:21 +0600

    fix: remove console.log

--- src/lib/server/hooks/private-route.ts
+++ src/lib/server/hooks/private-route.ts
@@ -4,11 +4,8 @@ import { pathToRegexp } from "path-to-regexp";
 const privateRouteHandler: Handle = async ({ event, resolve }) => {
 	const matcher = ["/app/:path*"];
 
-	console.log(pathToRegexp(matcher).test(event.url.pathname));
-
 	if (pathToRegexp(matcher).test(event.url.pathname)) {
 		const user = await event.locals.getSession();
-		console.log(user);
 		if (!user) throw redirect(307, "/login");
 	}
 
