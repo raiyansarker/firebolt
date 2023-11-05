f32c0de23de14f67398d37b46f21de2e6acdeb4b
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-06 00:33:29 +0600

    fix: refactor layout styles for app and settings pages

--- src/routes/(app)/app/+layout.svelte
+++ src/routes/(app)/app/+layout.svelte
@@ -48,113 +48,118 @@
 {#if menuOpen}
 	<MobileMenu bind:menuOpen {menuItems} bind:menuOpenButton />
 {/if}
-<div
-	class="flex w-full flex-row items-center justify-between bg-highlight px-4 py-2 text-background/90 transition-all md:px-8 lg:px-14"
->
-	<div class="flex flex-row gap-x-10">
-		<h1 class="text-xl font-bold tracking-wide">{env.PUBLIC_APP_NAME}</h1>
-		<nav class="hidden flex-row items-center gap-x-2 md:flex">
-			{#each menuItems as item}
-				<a
-					class="rounded-sm px-2 py-1 text-xs font-medium outline-none ring-ring transition-colors hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground focus-visible:ring-1"
-					class:mainmenu__nav_link_active={item.href === $page.url.pathname}
-					href={item.href}
-				>
-					{item.name}
-				</a>
-			{/each}
-		</nav>
-	</div>
-	<div class="flex flex-row items-center gap-x-2">
-		<button
-			class={buttonVariants({
-				variant: "ghost",
-				size: "sm",
-				class: "hidden h-7 w-7 px-0 md:inline-flex"
-			})}
-		>
-			<SearchIcon class="h-4 w-4" />
-		</button>
-		<a
-			href="/app/settings"
-			class={buttonVariants({
-				variant: "ghost",
-				size: "sm",
-				class: "hidden h-7 w-7 px-0 md:inline-flex"
-			})}
-		>
-			<SettingsIcon class="h-4 w-4" />
-		</a>
-		<button
-			class={buttonVariants({
-				variant: "ghost",
-				size: "sm",
-				class: "hidden h-7 w-7 px-0 md:inline-flex"
-			})}
-		>
-			<BellIcon class="h-4 w-4" />
-		</button>
-		<DropdownMenu.Root
-			positioning={{
-				placement: "bottom-start"
-			}}
-		>
-			<DropdownMenu.Trigger
-				class="rounded-full outline-none ring-ring ring-offset-2 ring-offset-slate-950 focus-visible:ring-1"
-			>
-				<Avatar.Root class="h-7 w-7">
-					<Avatar.Image src={$page.data.session?.user.image} alt="User Image" />
-					<Avatar.Fallback class="text-xs text-foreground"
-						>{getShortName($page.data.session?.user.name ?? "Harry Potter")}</Avatar.Fallback
+<div class="w-full bg-highlight text-background/90 transition-all">
+	<div
+		class="mx-auto flex w-[calc(100vw_-_8%)] flex-row items-center justify-between py-2 md:w-[calc(100vw_-_15%)]"
+	>
+		<div class="flex flex-row gap-x-10">
+			<h1 class="text-xl font-bold tracking-wide">{env.PUBLIC_APP_NAME}</h1>
+			<nav class="hidden flex-row items-center gap-x-2 md:flex">
+				{#each menuItems as item}
+					<a
+						class="rounded-sm px-2 py-1 text-xs font-medium outline-none ring-ring transition-colors hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground focus-visible:ring-1"
+						class:mainmenu__nav_link_active={item.href === $page.url.pathname}
+						href={item.href}
 					>
-				</Avatar.Root>
-			</DropdownMenu.Trigger>
-			<DropdownMenu.Content class="[&>*]:text-xs">
-				<DropdownMenu.Label class="flex flex-col pr-8">
-					<h2 class="text-sm text-foreground">{$page.data.session?.user.name}</h2>
-					<p class="text-xs font-light text-muted-foreground">
-						{$page.data.session?.user.email}
-					</p>
-				</DropdownMenu.Label>
-				<DropdownMenu.Separator />
-				<DropdownMenu.Item class="cursor-pointer" href="/app/settings/profile"
-					>Profile</DropdownMenu.Item
+						{item.name}
+					</a>
+				{/each}
+			</nav>
+		</div>
+		<div class="flex flex-row items-center gap-x-2">
+			<button
+				class={buttonVariants({
+					variant: "ghost",
+					size: "sm",
+					class: "hidden h-7 w-7 px-0 md:inline-flex"
+				})}
+			>
+				<SearchIcon class="h-4 w-4" />
+			</button>
+			<a
+				href="/app/settings"
+				class={buttonVariants({
+					variant: "ghost",
+					size: "sm",
+					class: "hidden h-7 w-7 px-0 md:inline-flex"
+				})}
+			>
+				<SettingsIcon class="h-4 w-4" />
+			</a>
+			<button
+				class={buttonVariants({
+					variant: "ghost",
+					size: "sm",
+					class: "hidden h-7 w-7 px-0 md:inline-flex"
+				})}
+			>
+				<BellIcon class="h-4 w-4" />
+			</button>
+			<DropdownMenu.Root
+				positioning={{
+					placement: "bottom-start"
+				}}
+			>
+				<DropdownMenu.Trigger
+					class="rounded-full outline-none ring-ring ring-offset-2 ring-offset-slate-950 focus-visible:ring-1"
 				>
-				<DropdownMenu.Item class="cursor-pointer" href="/app/settings/billing"
-					>Billing</DropdownMenu.Item
-				>
-				<DropdownMenu.Separator />
-				<DropdownMenu.Item class="cursor-pointer" href="/app/team">Team</DropdownMenu.Item>
-				<DropdownMenu.Item class="cursor-pointer" href="/app/team/create">
-					Create Team
-					<DropdownMenu.Shortcut>
-						<PlusIcon class="h-4 w-4" />
-					</DropdownMenu.Shortcut>
-				</DropdownMenu.Item>
-				<DropdownMenu.Item class="cursor-pointer" href="/app/tickets">
-					Support
-					<DropdownMenu.Shortcut>
-						<HelpCircleIcon class="h-4 w-4" />
-					</DropdownMenu.Shortcut>
-				</DropdownMenu.Item>
-				<DropdownMenu.Separator />
-				<DropdownMenu.Item on:click={() => signOut()} class="cursor-pointer">
-					Logout
-					<DropdownMenu.Shortcut>
-						<LogOutIcon class="h-4 w-4" />
-					</DropdownMenu.Shortcut>
-				</DropdownMenu.Item>
-			</DropdownMenu.Content>
-		</DropdownMenu.Root>
-		<button
-			on:click={() => (menuOpen = true)}
-			bind:this={menuOpenButton}
-			class={cn(buttonVariants({ variant: "ghost", size: "sm", class: "h-7 w-7 px-0 md:hidden" }))}
-		>
-			<MenuIcon class="h-4 w-4" />
-		</button>
+					<Avatar.Root class="h-7 w-7">
+						<Avatar.Image src={$page.data.session?.user.image} alt="User Image" />
+						<Avatar.Fallback class="text-xs text-foreground"
+							>{getShortName($page.data.session?.user.name ?? "Harry Potter")}</Avatar.Fallback
+						>
+					</Avatar.Root>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content class="[&>*]:text-xs">
+					<DropdownMenu.Label class="flex flex-col pr-8">
+						<h2 class="text-sm text-foreground">{$page.data.session?.user.name}</h2>
+						<p class="text-xs font-light text-muted-foreground">
+							{$page.data.session?.user.email}
+						</p>
+					</DropdownMenu.Label>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item class="cursor-pointer" href="/app/settings/profile"
+						>Profile</DropdownMenu.Item
+					>
+					<DropdownMenu.Item class="cursor-pointer" href="/app/settings/billing"
+						>Billing</DropdownMenu.Item
+					>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item class="cursor-pointer" href="/app/team">Team</DropdownMenu.Item>
+					<DropdownMenu.Item class="cursor-pointer" href="/app/team/create">
+						Create Team
+						<DropdownMenu.Shortcut>
+							<PlusIcon class="h-4 w-4" />
+						</DropdownMenu.Shortcut>
+					</DropdownMenu.Item>
+					<DropdownMenu.Item class="cursor-pointer" href="/app/tickets">
+						Support
+						<DropdownMenu.Shortcut>
+							<HelpCircleIcon class="h-4 w-4" />
+						</DropdownMenu.Shortcut>
+					</DropdownMenu.Item>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item on:click={() => signOut()} class="cursor-pointer">
+						Logout
+						<DropdownMenu.Shortcut>
+							<LogOutIcon class="h-4 w-4" />
+						</DropdownMenu.Shortcut>
+					</DropdownMenu.Item>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+			<button
+				on:click={() => (menuOpen = true)}
+				bind:this={menuOpenButton}
+				class={cn(
+					buttonVariants({ variant: "ghost", size: "sm", class: "h-7 w-7 px-0 md:hidden" })
+				)}
+			>
+				<MenuIcon class="h-4 w-4" />
+			</button>
+		</div>
 	</div>
 </div>
+
 <slot />
 
 <style lang="postcss">
--- src/routes/(app)/app/settings/+layout.svelte
+++ src/routes/(app)/app/settings/+layout.svelte
@@ -30,30 +30,32 @@
 	];
 </script>
 
-<div
-	class="w-full space-y-0.5 bg-highlight px-4 pt-4 text-background/90 transition-all md:space-y-1 md:px-8 lg:space-y-4 lg:px-14 lg:pt-8"
->
-	<h2 class="text-xl font-semibold md:text-2xl lg:text-3xl">Settings</h2>
-	<nav class="flex flex-row items-center gap-x-4 overflow-x-auto">
-		{#each menuItems as item}
-			<a
-				class={cn(
-					"relative py-3 text-xs text-background/60 hover:text-background/90 focus-visible:text-background/90 focus-visible:outline-none",
-					{
-						"!text-background/90": $page.url.pathname === item.href
-					}
-				)}
-				href={item.href}
-			>
-				{item.name}
-				{#if $page.url.pathname === item.href}
-					<span class="absolute inset-0 bottom-0 border-b-2 border-b-background/90" />
-				{/if}
-			</a>
-		{/each}
-	</nav>
+<div class="bg-highlight pt-4 text-background/90 transition-all lg:pt-8">
+	<div
+		class="mx-auto w-[calc(100vw_-_8%)] space-y-0.5 md:w-[calc(100vw_-_15%)] md:space-y-1 lg:space-y-4"
+	>
+		<h2 class="text-xl font-semibold md:text-2xl lg:text-3xl">Settings</h2>
+		<nav class="flex flex-row items-center gap-x-4 overflow-x-auto">
+			{#each menuItems as item}
+				<a
+					class={cn(
+						"relative py-3 text-xs text-background/60 hover:text-background/90 focus-visible:text-background/90 focus-visible:outline-none",
+						{
+							"!text-background/90": $page.url.pathname === item.href
+						}
+					)}
+					href={item.href}
+				>
+					{item.name}
+					{#if $page.url.pathname === item.href}
+						<span class="absolute inset-0 bottom-0 border-b-2 border-b-background/90" />
+					{/if}
+				</a>
+			{/each}
+		</nav>
+	</div>
 </div>
 
-<div class="my-6 px-4 md:px-8 lg:my-12 lg:px-14">
+<div class="mx-auto my-6 w-[calc(100vw_-_8%)] md:w-[calc(100vw_-_15%)] lg:my-12">
 	<slot />
 </div>
