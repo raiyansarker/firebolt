4fa2f3c441bfb5be14be79f7df9fb1fe15acd66e
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-08 20:52:09 +0600

    feat: domains page basic design (unfinished)

---
+++ src/routes/(app)/app/domains/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { env } from "$env/dynamic/public";
+	import { Button } from "$lib/components/ui/button";
+	import DomainTable from "./table.svelte";
+	import { Separator } from "$lib/components/ui/separator";
+	import PlusIcon from "~icons/lucide/plus";
+</script>
+
+<div class="mx-auto my-6 w-[calc(100vw_-_8%)] md:w-[calc(100vw_-_15%)] lg:my-12">
+	<div class="flex w-full flex-row items-start justify-between">
+		<div>
+			<h2 class="text-xl font-bold md:text-2xl">Domain Management</h2>
+			<p class="text-sm leading-relaxed text-muted-foreground">
+				Manage your domain and configure it to connect with {env.PUBLIC_APP_NAME}
+			</p>
+		</div>
+		<Button variant="outline" class="gap-x-2 px-4 text-sm">
+			<PlusIcon class="h-5 w-5" />
+			Add domain
+		</Button>
+	</div>
+	<Separator class="my-3 lg:my-6" />
+	<div class="flex flex-col items-start justify-between gap-6 md:flex-row">
+		<div class="w-full md:w-1/3">
+			<h3 class="text-sm font-medium">Domains</h3>
+			<p class="text-xs leading-relaxed text-muted-foreground">
+				Manage your domain and configure it to connect with {env.PUBLIC_APP_NAME}
+			</p>
+		</div>
+		<DomainTable />
+	</div>
+</div>
---
+++ src/routes/(app)/app/domains/table.svelte
@@ -0,0 +1,1 @@
+<h1 class="w-full">Hello</h1>
