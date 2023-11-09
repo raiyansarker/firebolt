f868bd3cfb79887cb6cd4f3b9a560d49f93f7d59
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-09 07:38:53 +0600

    feat: add create domain page with back button and placeholder message

---
+++ src/routes/(app)/app/domains/create/+page@.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { buttonVariants } from "$lib/components/ui/button";
+	import ChevronLeftIcon from "~icons/lucide/chevron-left";
+</script>
+
+<div class="h-screen w-screen bg-background">
+	<a
+		class={buttonVariants({ variant: "ghost", class: "absolute left-6 top-6 gap-x-1 pl-2" })}
+		href="/app/domains"
+	>
+		<ChevronLeftIcon class="h-5 w-5" />
+		Back
+	</a>
+	<div class="grid h-screen w-screen place-items-center">Not available at the moment</div>
+</div>
