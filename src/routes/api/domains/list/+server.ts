ee91c41078c863eb2415629b325b056c6295cc95
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-09 07:37:32 +0600

    feat: add DomainStatus and select query parameter to domain list API endpoint

--- src/routes/api/domains/list/+server.ts
+++ src/routes/api/domains/list/+server.ts
@@ -1,31 +1,49 @@
 import { db } from "$lib/server/db";
-import { domains } from "$lib/server/db/schema";
+import { DomainStatus, domains, type DomainsModel } from "$lib/server/db/schema";
 import { error, json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 import { fromZodError } from "zod-validation-error";
+import { zodEnhanced, type InferPartialSelect } from "$lib/utils";
 
 export const GET: RequestHandler = async ({ locals, url: { searchParams } }) => {
 	const session = await locals.getSession();
 	if (!session) throw error(401);
 
 	const routeSchema = z.object({
-		limit: z.coerce.number().nonnegative().max(100).optional().default(10)
+		limit: z.coerce.number().nonnegative().max(100).optional().default(10),
+		status: z.enum(DomainStatus).optional(),
+		select: zodEnhanced<InferPartialSelect<Omit<DomainsModel, "ownerId">>>()
+			.with({
+				id: z.boolean().default(true).optional(),
+				name: z.boolean().optional(),
+				status: z.boolean().optional(),
+				createdAt: z.boolean().optional(),
+				updatedAt: z.boolean().optional()
+			})
+			.optional()
 	});
 
+	const encodedSearchParam = searchParams.get("select") ?? undefined;
+
 	const parse = routeSchema.safeParse({
-		limit: searchParams.get("limit")
+		limit: searchParams.get("limit") ?? undefined,
+		status: searchParams.get("status") ?? undefined,
+		select: encodedSearchParam && JSON.parse(encodedSearchParam)
 	});
 	if (!parse.success) throw error(400, fromZodError(parse.error));
 
 	/**
 	 * Destructure all available query parameters
 	 */
-	const { limit } = parse.data;
+	const { limit, status, select } = parse.data;
 
 	const data = await db.query.domains.findMany({
-		where: eq(domains.ownerId, session.user.id),
+		columns: select && {
+			...select
+		},
+		where: and(eq(domains.ownerId, session.user.id), status && eq(domains.status, status)),
 		limit // if limit set to 0, drizzle won't use limit
 	});
 
