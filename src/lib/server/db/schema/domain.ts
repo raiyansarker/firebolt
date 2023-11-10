333b6be640f77746745317ddfaed4f21c14e4d30
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-11 02:32:56 +0600

    fix: update domain and link schema

--- src/lib/server/db/schema/domain.ts
+++ src/lib/server/db/schema/domain.ts
@@ -1,30 +1,36 @@
 import { type InferSelectModel, relations, sql } from "drizzle-orm";
 import { createId } from "@paralleldrive/cuid2";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { users } from "./user";
 
-export const DomainStatus = ["active", "hold", "banned"] as const;
+export const DomainStatus = ["active", "pending", "hold", "banned"] as const;
 export type DomainStatusType = (typeof DomainStatus)[number];
 
-export const domains = sqliteTable("domain", {
-	id: text("id")
-		.notNull()
-		.primaryKey()
-		.$defaultFn(() => createId()),
-	ownerId: text("owner_id").notNull(),
-	name: text("name").unique().notNull(),
-	status: text("status", { enum: DomainStatus })
-		.notNull()
-		.$type<DomainStatusType>()
-		.default("active"),
-	shared: integer("shared", { mode: "boolean" }).default(false),
-	createdAt: integer("createdAt", { mode: "timestamp_ms" })
-		.notNull()
-		.default(sql`CURRENT_TIMESTAMP`),
-	updatedAt: integer("updatedAt", { mode: "timestamp_ms" })
-		.notNull()
-		.default(sql`CURRENT_TIMESTAMP`)
-});
+export const domains = sqliteTable(
+	"domain",
+	{
+		id: text("id")
+			.notNull()
+			.primaryKey()
+			.$defaultFn(() => createId()),
+		ownerId: text("owner_id").notNull(),
+		name: text("name").unique().notNull(),
+		status: text("status", { enum: DomainStatus })
+			.notNull()
+			.$type<DomainStatusType>()
+			.default("pending"),
+		shared: integer("shared", { mode: "boolean" }).default(false),
+		createdAt: integer("createdAt", { mode: "timestamp_ms" })
+			.notNull()
+			.default(sql`CURRENT_TIMESTAMP`),
+		updatedAt: integer("updatedAt", { mode: "timestamp_ms" })
+			.notNull()
+			.default(sql`CURRENT_TIMESTAMP`)
+	},
+	(table) => ({
+		ownerIdx: index("owner_idx").on(table.ownerId)
+	})
+);
 
 export const domainsRelations = relations(domains, ({ one }) => ({
 	owner: one(users, {
--- src/lib/server/db/schema/link.ts
+++ src/lib/server/db/schema/link.ts
@@ -1,6 +1,6 @@
 import { type InferSelectModel, relations, sql } from "drizzle-orm";
 import { createId } from "@paralleldrive/cuid2";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { users } from "./user";
 import { domains } from "./domain";
 import { nanoid } from "nanoid";
@@ -8,26 +8,36 @@ import { nanoid } from "nanoid";
 export const LinkStatus = ["active", "hold", "banned"] as const;
 export type LinkStatusType = (typeof LinkStatus)[number];
 
-export const links = sqliteTable("link", {
-	id: text("id")
-		.notNull()
-		.primaryKey()
-		.$defaultFn(() => createId()),
-	ownerId: text("owner_id").notNull(),
-	domainId: text("domain_id").notNull(),
-	shortUrl: text("short_url")
-		.unique()
-		.notNull()
-		.$defaultFn(() => nanoid(6)),
-	destinationUrl: text("destinationUrl").notNull(),
-	status: text("status", { enum: LinkStatus }).notNull().$type<LinkStatusType>().default("active"),
-	createdAt: integer("createdAt", { mode: "timestamp_ms" })
-		.notNull()
-		.default(sql`CURRENT_TIMESTAMP`),
-	updatedAt: integer("updatedAt", { mode: "timestamp_ms" })
-		.notNull()
-		.default(sql`CURRENT_TIMESTAMP`)
-});
+export const links = sqliteTable(
+	"link",
+	{
+		id: text("id")
+			.notNull()
+			.primaryKey()
+			.$defaultFn(() => createId()),
+		ownerId: text("owner_id").notNull(),
+		domainId: text("domain_id").notNull(),
+		key: text("key")
+			.notNull()
+			.$defaultFn(() => nanoid(6)),
+		url: text("url").notNull(),
+		status: text("status", { enum: LinkStatus })
+			.notNull()
+			.$type<LinkStatusType>()
+			.default("active"),
+		createdAt: integer("createdAt", { mode: "timestamp_ms" })
+			.notNull()
+			.default(sql`CURRENT_TIMESTAMP`),
+		updatedAt: integer("updatedAt", { mode: "timestamp_ms" })
+			.notNull()
+			.default(sql`CURRENT_TIMESTAMP`)
+	},
+	(table) => ({
+		keyIdx: index("key_idx").on(table.key),
+		ownerIdx: index("owner_idx").on(table.ownerId),
+		domainIdx: index("domain_idx").on(table.domainId)
+	})
+);
 
 export const linksRelations = relations(links, ({ one }) => ({
 	owner: one(users, {
