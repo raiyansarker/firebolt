a9bc0f064f0602ad70a49f337a2633c4e5e15e70
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-11 02:29:19 +0600

    chore: format

--- src/lib/components/ui/dropdown-menu/dropdown-menu-separator.svelte
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-separator.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import { DropdownMenu as DropdownMenuPrimitive } from 'bits-ui';
-	import { cn } from '$lib/utils';
+	import { DropdownMenu as DropdownMenuPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils";
 
 	type $$Props = DropdownMenuPrimitive.SeparatorProps;
 
-	let className: $$Props['class'] = undefined;
+	let className: $$Props["class"] = undefined;
 	export { className as class };
 </script>
 
 <DropdownMenuPrimitive.Separator
-	class={cn('-mx-2 my-2 h-px bg-muted', className)}
+	class={cn("-mx-2 my-2 h-px bg-muted", className)}
 	{...$$restProps}
 />
