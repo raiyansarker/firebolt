0fb70064b25bb550d1c1584a390e025154bcf3ca
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-13 12:17:52 +0600

    fix: update page layout width in pageshell component

--- src/lib/components/ui/page/pageshell.svelte
+++ src/lib/components/ui/page/pageshell.svelte
@@ -4,7 +4,7 @@
 
 <div
 	class={cn(
-		"mx-auto my-6 w-[calc(100vw_-_8%)] md:w-[calc(100vw_-_15%)] lg:my-12",
+		"mx-auto my-6 w-[calc(100vw_-_8%)] md:w-[calc(100vw_-_10%)] lg:my-12",
 		$$restProps.class
 	)}
 >
