8ee6a6a91d84e657df7e5808dc2b311458b8617b
Raiyan Sarker <developerraiyan@gmail.com>
2023-11-16 07:43:20 +0600

    feat: add password hashing for link creation

--- src/routes/(app)/app/links/create/+page.server.ts
+++ src/routes/(app)/app/links/create/+page.server.ts
@@ -7,9 +7,10 @@ import { and, eq } from "drizzle-orm";
 import { nanoid } from "nanoid";
 import { message, superValidate } from "sveltekit-superforms/server";
 import { z } from "zod";
+import { hashSync } from "bcryptjs";
 import type { PageServerLoad } from "./$types";
 
-const _linkCreateSchema = zodEnhanced<
+const linkCreateSchema = zodEnhanced<
 	Pick<LinksModel, "url" | "password" | "expire" | "domainId"> & Partial<Pick<LinksModel, "key">>
 >().with({
 	url: z.string().url(),
@@ -37,7 +38,7 @@ export const load: PageServerLoad = async ({ locals }) => {
 				.where(and(eq(domainsTable.shared, true), eq(domainsTable.status, "active")))
 		);
 
-	const form = await superValidate(_linkCreateSchema);
+	const form = await superValidate(linkCreateSchema);
 
 	return {
 		form,
@@ -49,10 +50,7 @@ export const actions = {
 	default: async ({ request, locals }) => {
 		const session = await locals.getSession();
 		if (!session) throw error(401);
-		const form = await superValidate(await request.formData(), _linkCreateSchema);
-
-		// create artificial delay of 10 seconds
-		// await new Promise((resolve) => setTimeout(resolve, 10000));
+		const form = await superValidate(await request.formData(), linkCreateSchema);
 
 		if (!form.valid) {
 			/**
@@ -95,12 +93,14 @@ export const actions = {
 		}
 
 		const key = form.data.key ?? nanoid(Number(env.LINK_DEFAULT_SIZE ?? 6));
+		const password = form.data.password ? hashSync(form.data.password) : null; // never store plain password
 		const data = await db
 			.insert(links)
 			.values({
 				...form.data,
 				key: key,
-				ownerId: session.user.id
+				ownerId: session.user.id,
+				password
 			})
 			.catch((error) => {
 				console.log(error);
